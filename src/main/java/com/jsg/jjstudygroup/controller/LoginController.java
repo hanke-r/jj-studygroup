f2a0ad2837225ed5a5285eed656453d5dadec7b8
han-ker <hanjaeok17@gmail.com>
2021-09-07 23:27:10 +0900

    2021. 09. 07 application.properties  - hibernate 설정    -. camelcase, underscore 기법  - sql 콘솔 출력
    html
     - thymeleaf 설정
     - main : content, footer 내용 삭제
     - login : footer 삭제

--- src/main/java/com/jsg/jjstudygroup/controller/LoginController.java
+++ src/main/java/com/jsg/jjstudygroup/controller/LoginController.java
@@ -1,12 +1,17 @@
 package com.jsg.jjstudygroup.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping("/login")
+    /**
+     * login method
+     *
+     * @return login
+     */
+    @GetMapping("/login")
     public String login(){
         return "login";
     }
