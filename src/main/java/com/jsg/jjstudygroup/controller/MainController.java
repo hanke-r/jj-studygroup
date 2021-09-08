33472117b5cb69440a71dc78a6a4d85e96c587ba
han-ker <hanjaeok17@gmail.com>
2021-09-09 00:15:49 +0900

    2021. 09. 09  - 다국어 처리 (html select box)   -. jQuery 처리 완료   -. select box change event

--- src/main/java/com/jsg/jjstudygroup/controller/MainController.java
+++ src/main/java/com/jsg/jjstudygroup/controller/MainController.java
@@ -2,6 +2,14 @@ package com.jsg.jjstudygroup.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 @Controller
 public class MainController {
@@ -15,4 +23,5 @@ public class MainController {
     public String main(){
         return "index";
     }
+
 }
